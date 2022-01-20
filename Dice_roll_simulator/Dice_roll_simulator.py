5418e57eb942c3a85f0f531baf6ff1bd4057f702
ranjanbipul257 <78407413+ranjanbipul257@users.noreply.github.com>
2022-01-20 22:23:23 +0530

    Add files via upload

---
+++ Dice_roll_simulator/Dice_roll_simulator.py
@@ -0,0 +1,12 @@
+
+import random
+while True:
+    print("1.roll the dice   2.exit")
+    user=int(input("what you want to do\n"))
+    if user==1:
+        number=random.randint(1,6)
+        print("the no. is",number)
+
+    else:
+        break
+
